c45d17a5c0e1647fa9a420df8cac20e8d90ed06a
dkuzmich <312905dm>
2017-03-29 12:57:19 +0300

    Update servlet add jsp + remove WEB-inf

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,13 +9,17 @@ import java.io.PrintWriter;
 /**
  * Created by dkuzmich on 3/28/2017.
  */
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
-        PrintWriter pw=resp.getWriter();
-        pw.print("<h1> Hello servlet </h1>");
+        //PrintWriter pw=resp.getWriter();
+        //pw.print("<h1> Hello servlet </h1>");
+        req.setAttribute("name", "Dima");
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+
     }
 
     @Override
---
+++ src/main/webapp/mypage.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dkuzmich
+  Date: 3/29/2017
+  Time: 11:52 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>MyTestServlet</title>
+</head>
+
+
+<body>
+<h1>Hello my servlet! and hello ${name} </h1>
+</body>
+
+</html>
